f314e482b5e4220c3d8303872e345fd1c7b518f6
Matheus <matheusll@id.uff.br>
2023-05-28 18:11:34 -0300

    yes

--- client/client.py
+++ client/client.py
@@ -1,26 +1,129 @@
-import sys
-import socket
-import selectors
-import types
+import threading, socket, sys, os
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
-sel = selectors.DefaultSelector()
-messages = [b"Message 1 from client.", b"Message 2 from client."]
+HEADERSIZE = 10
+MUSICFOLDER = "./music"
 
-def start_connections(host, port, num_conns):
-    server_addr = (host, port)
-    for i in range(0, num_conns):
-        connid = i + 1
-        print(f"Starting connection {connid} to {server_addr}")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        sock.connect_ex(server_addr)
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        data = types.SimpleNamespace(
-            connid=connid,
-            msg_total=sum(len(m) for m in messages),
-            recv_total=0,
-            messages=messages.copy(),
-            outb=b"",
-        )
-        sel.register(sock, events, data=data)
+class Program():
+    def __init__(self, pair):
+        self.app = QApplication([])
+        self.mainwindow = QMainWindow()
+        self.mainwindow.setWindowTitle('p2psmp')
+        self.mainwindow.resize(700, 400)
 
+
+
+
+        self.pair = pair
+        self.sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sckt.connect(self.pair)
+        threading.Thread(target=self.recv).start()
+        #threading.Thread(target=self.send).start()
+        # MORE BS
+        self.spawnInfoDialog("Connected to the server!")
+
+        # ui bullshit
+        # ui bullshit
+        # ui bullshit
+
+        self.outerLayout = QHBoxLayout()
+
+        self.layout = QHBoxLayout()
+        self.listView = QListView()
+        self.model = QStandardItemModel()
+        self.listView.setModel(self.model)
+        self.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.listView.setObjectName("Song List")
+
+        self.rightLayout = QVBoxLayout()
+        self.playButton = QPushButton('Play File', self.mainwindow)
+        self.exposeButton = QPushButton('Expose Files', self.mainwindow)
+        self.exposeButton.clicked.connect(self.exposeAllFiles)
+        self.updateButton = QPushButton('Refresh Files', self.mainwindow)
+        self.updateButton.clicked.connect(self.loadFileList)
+        self.rightLayout.addWidget(self.playButton)
+        self.rightLayout.addWidget(self.exposeButton)
+        self.rightLayout.addWidget(self.updateButton)
+        self.rightLayout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding))
+
+        self.layout.addWidget(self.listView)
+        self.outerLayout.addLayout(self.layout)
+        self.outerLayout.addLayout(self.rightLayout)
+        
+        self.widget = QWidget()
+        self.widget.setLayout(self.outerLayout)
+        self.mainwindow.setCentralWidget(self.widget)
+        self.mainwindow.show()
+
+        self.app.aboutToQuit.connect(self.disconnect)
+        self.app.exec()
+
+    def exposeAllFiles(self):
+        self.customSend(self.getPDU('export ' + ';;;'.join(self.getMySongs())))
+
+    def loadFileList(self):
+        self.customSend(self.getPDU('list'))
+
+    def getMySongs(self):
+        filelist = []
+        for root, dirs, files in os.walk(MUSICFOLDER):
+            for file in files:
+                if file.endswith('.mp3'):
+                    filelist.append(os.path.join(root, file)[8:])
+        return filelist
+
+    def spawnInfoDialog(self, text):
+        dlg = QMessageBox(self.mainwindow)
+        dlg.setWindowTitle('p2psmp')
+        dlg.setText(text)
+        dlg.exec()
+
+    def disconnect(self):
+        self.customSend(self.getPDU('close'))
+
+    def getPDU(self, data):
+        return bytes(f"{len(data):<{HEADERSIZE}}" + data, 'utf-8')
+
+    def recv(self):
+        full_msg = ''
+        new_msg = True
+        while True:
+            msg = self.sckt.recv(1024)
+            if new_msg:
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
+            full_msg += msg.decode("utf-8")
+            while len(full_msg[HEADERSIZE:msglen + HEADERSIZE]) == msglen:
+                new_msg = True
+                if full_msg[HEADERSIZE:msglen + HEADERSIZE] == "close":
+                    self.sckt.close()
+                    print("Disconnected from the server!")
+                    exit()
+                elif full_msg[HEADERSIZE:][:4] == "list":
+                    songList = full_msg[HEADERSIZE:][5:].split(';;;')
+                    self.model.clear()
+                    for song in songList:
+                        self.model.appendRow(QStandardItem(song))
+                else: 
+                    print(full_msg[HEADERSIZE:msglen + HEADERSIZE])
+                full_msg = full_msg[HEADERSIZE + msglen:]
+
+    def send(self):
+        while True:
+            data = input()
+            if data == 'close':
+                self.sckt.send(self.getPDU(data))
+                break
+            elif data[:7] == 'export ':
+                filelist = []
+                if data[7:] == '*':
+                    filelist = self.getMySongs()
+                    data = data.replace('*', ';;;'.join(filelist))
+            self.sckt.send(self.getPDU(data))
+
+    def customSend(self, pdu):
+        self.sckt.send(pdu)
+
+program = Program((sys.argv[1], int(sys.argv[2])))
--- server/server.py
+++ server/server.py
@@ -1,53 +1,73 @@
-import sys
 import socket
-import selectors
-import types
+import sys
+import threading
 
-sel = selectors.DefaultSelector()
-host, port = sys.argv[1], int(sys.argv[2])
-sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sckt.bind((host, port))
-sckt.listen()
-sckt.setblocking(False)
-sel.register(sckt, selectors.EVENT_READ, data=None)
+HEADERSIZE = 10
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-# multiconn-server.py
+class Socket():
+    userCount = 0
+    clientIDs = {}
+    clients = []
 
-# ...
+    def __init__(self, pair):
+        self.pair = pair
+        self.sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sckt.bind(self.pair)
+        self.sckt.listen()
+        threading.Thread(target=self.accept).start()
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print(f"Closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+    def getSongList(self):
+        songList = []
+        for x in range(len(self.clients)):
+            for y in range(len(self.clients[x][1])):
+                if self.clients[x][2]:
+                    songList.append(str(x) + ' ' + self.clients[x][1][y])
+        return songList
 
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
+    def getPDU(self, payload):
+        return bytes(f"{len(payload):<{HEADERSIZE}}" + payload, 'utf-8')
+
+    def accept(self):
+        while True:
+            client, address = self.sckt.accept()
+            if(address in self.clientIDs):
+                self.clients[self.clientIDs.get(address)][2] = True
+                client.send(self.getPDU(f"Connected to the server."))
             else:
-                service_connection(key, mask)
-except KeyboardInterrupt:
-    print("Shutting down server...")
-finally:
-    sel.close()
+                self.clients.append([(client, address), [], True])
+                self.clientIDs[address] = self.userCount
+                self.userCount += 1
+                client.send(self.getPDU(f"Connected to the server."))
+                client.send(self.getPDU(f"Successfully registered."))
+            threading.Thread(target=self.recvfrom, args=(client, address,)).start()
+            print(f">> {address}")
+
+    def recvfrom(self, connection, address):
+        full_msg = ''
+        new_msg = True
+        while True:
+            msg = connection.recv(1024)
+            if new_msg:
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
+            full_msg += msg.decode("utf-8")
+            while len(full_msg[HEADERSIZE:msglen + HEADERSIZE]) == msglen:
+                new_msg = True
+                if full_msg[HEADERSIZE:msglen + HEADERSIZE] == 'close':
+                    connection.send(self.getPDU('close'))
+                    connection.close()
+                    self.clients[self.clientIDs.get(address)][2] = False
+                    print(f"<< {address}")
+                    break
+                elif full_msg[HEADERSIZE:][:6] == 'export':
+                    songlist = full_msg[HEADERSIZE:][7:].split(';;;')
+                    self.clients[self.clientIDs.get(address)][1] = songlist
+                elif full_msg[HEADERSIZE:][:4] == 'list':
+                    connection.send(self.getPDU('list ' + ';;;'.join(self.getSongList())))
+                #print(full_msg[HEADERSIZE:msglen + HEADERSIZE])
+                full_msg = full_msg[HEADERSIZE + msglen:]
+
+    def send(self, connection, pdu):
+        connection.send(pdu)
+
+sckt = Socket((sys.argv[1], int(sys.argv[2])))
