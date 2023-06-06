31518b0c500c40cbbfeb4b851db8db72e55926f6
Matheus <matheusll@id.uff.br>
2023-06-06 02:29:32 -0300

    added comments

--- client/client.py
+++ client/client.py
@@ -3,7 +3,10 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+# tamanho do header que contém o tamanho da mensagem
 HEADERSIZE = 10
+
+# caminho para a pasta de músicas
 MUSICFOLDER = "./music"
 
 class Program():
@@ -13,41 +16,47 @@ class Program():
         self.mainwindow.setWindowTitle('p2psmp')
         self.mainwindow.resize(700, 400)
 
-
-
-
         self.pair = pair
         self.sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sckt.connect(self.pair)
-        threading.Thread(target=self.recv).start()
+        self.error = False
+        self.recv(True)
+        threading.Thread(target=self.recv, args=(False,)).start()
         #threading.Thread(target=self.send).start()
-        # MORE BS
-        self.spawnInfoDialog("Connected to the server!")
-
-        # ui bullshit
-        # ui bullshit
-        # ui bullshit
+        if not self.error:
+            self.spawnInfoDialog("Connected to the server!")
+        else:
+            self.spawnInfoDialog("User already registered, exiting...")
+            self.mainwindow.close()
+        self.error = 'passed'
 
         self.outerLayout = QHBoxLayout()
 
-        self.layout = QHBoxLayout()
+        self.layout = QVBoxLayout()
         self.listView = QListView()
-        self.model = QStandardItemModel()
-        self.listView.setModel(self.model)
+        self.userView = QListView()
+        self.modelSong = QStandardItemModel()
+        self.modelUser = QStandardItemModel()
+        self.listView.setModel(self.modelSong)
         self.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.listView.setObjectName("Song List")
+        self.userView.setModel(self.modelUser)
+        self.userView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.userView.setObjectName("User List")
+        self.userView.setMaximumHeight(75)
 
         self.rightLayout = QVBoxLayout()
         self.playButton = QPushButton('Play File', self.mainwindow)
         self.exposeButton = QPushButton('Expose Files', self.mainwindow)
         self.exposeButton.clicked.connect(self.exposeAllFiles)
-        self.updateButton = QPushButton('Refresh Files', self.mainwindow)
+        self.updateButton = QPushButton('Refresh', self.mainwindow)
         self.updateButton.clicked.connect(self.loadFileList)
         self.rightLayout.addWidget(self.playButton)
         self.rightLayout.addWidget(self.exposeButton)
         self.rightLayout.addWidget(self.updateButton)
         self.rightLayout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding))
-
+        
+        self.layout.addWidget(self.userView)
         self.layout.addWidget(self.listView)
         self.outerLayout.addLayout(self.layout)
         self.outerLayout.addLayout(self.rightLayout)
@@ -60,12 +69,15 @@ class Program():
         self.app.aboutToQuit.connect(self.disconnect)
         self.app.exec()
 
+    # envia ao servidor a própria lista de arquivos
     def exposeAllFiles(self):
         self.customSend(self.getPDU('export ' + ';;;'.join(self.getMySongs())))
 
+    # pede ao servidor a lista de arquivos disponíveis
     def loadFileList(self):
         self.customSend(self.getPDU('list'))
 
+    # anda pela pasta de músicas especificadas e retorna uma lista pro caminho de cada uma delas
     def getMySongs(self):
         filelist = []
         for root, dirs, files in os.walk(MUSICFOLDER):
@@ -74,19 +86,23 @@ class Program():
                     filelist.append(os.path.join(root, file)[8:])
         return filelist
 
+    # spawna um dialog
     def spawnInfoDialog(self, text):
         dlg = QMessageBox(self.mainwindow)
         dlg.setWindowTitle('p2psmp')
         dlg.setText(text)
         dlg.exec()
 
+    # pede para desconectar do servidor
     def disconnect(self):
         self.customSend(self.getPDU('close'))
 
+    # encapsula o payload
     def getPDU(self, data):
         return bytes(f"{len(data):<{HEADERSIZE}}" + data, 'utf-8')
 
-    def recv(self):
+    # recebe mensagens novas do servidor, pode ser executado constantemente ou apenas uma vez (once = True)
+    def recv(self, once):
         full_msg = ''
         new_msg = True
         while True:
@@ -99,31 +115,48 @@ class Program():
                 new_msg = True
                 if full_msg[HEADERSIZE:msglen + HEADERSIZE] == "close":
                     self.sckt.close()
-                    print("Disconnected from the server!")
+                    if self.error == False:
+                        print("User already registered!")
+                    else:
+                        print("Disconnected from the server!")
+                    self.error = True
                     exit()
                 elif full_msg[HEADERSIZE:][:4] == "list":
                     songList = full_msg[HEADERSIZE:][5:].split(';;;')
-                    self.model.clear()
+                    self.modelSong.clear()
                     for song in songList:
-                        self.model.appendRow(QStandardItem(song))
+                        self.modelSong.appendRow(QStandardItem(song))
+                elif full_msg[HEADERSIZE:][:4] == "user":
+                    userList = full_msg[HEADERSIZE:][5:].split(';;;')
+                    self.modelUser.clear()
+                    for user in userList:
+                        self.modelUser.appendRow(QStandardItem(user))
                 else: 
                     print(full_msg[HEADERSIZE:msglen + HEADERSIZE])
                 full_msg = full_msg[HEADERSIZE + msglen:]
-
+                if len(full_msg) > 10:
+                    msglen = int(full_msg[:HEADERSIZE])
+            if once: 
+                break
+    
+    # recebe input do usuário constantemente e envia para o servidor (não é usado com interface gráfica)
     def send(self):
         while True:
             data = input()
+            filelist = []
             if data == 'close':
                 self.sckt.send(self.getPDU(data))
                 break
             elif data[:7] == 'export ':
-                filelist = []
                 if data[7:] == '*':
                     filelist = self.getMySongs()
                     data = data.replace('*', ';;;'.join(filelist))
-            self.sckt.send(self.getPDU(data))
+            if filelist != []:
+                self.sckt.send(self.getPDU(data))
 
+    # envia mensagens específicas com a pdu como parâmetro
     def customSend(self, pdu):
         self.sckt.send(pdu)
 
+# parâmetro 1 = ip, 2 = porta (do servidor)
 program = Program((sys.argv[1], int(sys.argv[2])))
--- server/server.py
+++ server/server.py
@@ -2,11 +2,14 @@ import socket
 import sys
 import threading
 
+# tamanho do header que contém o tamanho da mensagem
 HEADERSIZE = 10
 
 class Socket():
     userCount = 0
+    # associa um número a cada usuário
     clientIDs = {}
+    # [(conexão, (ip,porta)), lista de músicas, ativo]
     clients = []
 
     def __init__(self, pair):
@@ -16,32 +19,40 @@ class Socket():
         self.sckt.listen()
         threading.Thread(target=self.accept).start()
 
+    # retorna lista de músicas apenas dos usuários ativos
     def getSongList(self):
         songList = []
         for x in range(len(self.clients)):
             for y in range(len(self.clients[x][1])):
-                if self.clients[x][2]:
-                    songList.append(str(x) + ' ' + self.clients[x][1][y])
+                if(self.clients[x][2] and self.clients[x][1] != []):
+                    songList.append(self.clients[x][1][y])
         return songList
 
+    # encapsula o payload
     def getPDU(self, payload):
         return bytes(f"{len(payload):<{HEADERSIZE}}" + payload, 'utf-8')
 
+    # aceita conexões novas a todo momento
     def accept(self):
         while True:
+            connected = False
             client, address = self.sckt.accept()
-            if(address in self.clientIDs):
-                self.clients[self.clientIDs.get(address)][2] = True
-                client.send(self.getPDU(f"Connected to the server."))
-            else:
+            # checa se o cliente já existe e está ativo
+            for clientI in self.clients:
+                if clientI[0][1][0] == address[0] and clientI[2]:
+                    connected = True
+                    client.send(self.getPDU('close'))
+                    client.close()
+            if not connected:
                 self.clients.append([(client, address), [], True])
                 self.clientIDs[address] = self.userCount
                 self.userCount += 1
                 client.send(self.getPDU(f"Connected to the server."))
                 client.send(self.getPDU(f"Successfully registered."))
-            threading.Thread(target=self.recvfrom, args=(client, address,)).start()
-            print(f">> {address}")
+                threading.Thread(target=self.recvfrom, args=(client, address,)).start()
+                print(f">> {address}")
 
+    # recebe mensagens novas de uma conexão específica
     def recvfrom(self, connection, address):
         full_msg = ''
         new_msg = True
@@ -64,10 +75,19 @@ class Socket():
                     self.clients[self.clientIDs.get(address)][1] = songlist
                 elif full_msg[HEADERSIZE:][:4] == 'list':
                     connection.send(self.getPDU('list ' + ';;;'.join(self.getSongList())))
+                    clientList = []
+                    for client in self.clients:
+                        if client[2]:
+                            clientList.append(client[0][1][0])
+                    connection.send(self.getPDU('user ' + ';;;'.join(clientList)))
                 #print(full_msg[HEADERSIZE:msglen + HEADERSIZE])
                 full_msg = full_msg[HEADERSIZE + msglen:]
+                if len(full_msg) > 10:
+                    msglen = int(full_msg[:HEADERSIZE])
 
+    # envia mensagem para a conexão especificada
     def send(self, connection, pdu):
         connection.send(pdu)
 
+# parâmetro 1 = ip, 2 = porta (que o servidor vai rodar)
 sckt = Socket((sys.argv[1], int(sys.argv[2])))
